035af06122c29eeeea3eb0cf63dff9f5a6fb5041
Anderson Laventure <Laventurepsu1@gmail.com>
2025-08-08 20:12:10 -0400

    majority-element problem

---
+++ arrays-and-hashing/majority-element.ts
@@ -0,0 +1,28 @@
+// Majority Element
+// https://leetcode.com/problems/majority-element/
+
+function majorityElement(nums: number[]): number {
+  const container: { [key: number]: number } = {};
+  let mostCommonNumber = 0;
+  let highestCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (container[nums[i]] === undefined) {
+      container[nums[i]] = 1;
+    } else {
+      container[nums[i]] += 1;
+    }
+  }
+
+  for (let key in container) {
+    if (container[key] > highestCount) {
+      mostCommonNumber = Number(key);
+      highestCount = container[key];
+    }
+  }
+  return mostCommonNumber;
+}
+
+const testNums = [9, 9, 8];
+
+console.log(majorityElement(testNums));
