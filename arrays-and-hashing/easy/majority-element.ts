da34455598151ec8a9e747a775a2057141d4a9eb
Anderson Laventure <Laventurepsu1@gmail.com>
2025-08-17 20:57:09 -0400

    redid

--- arrays-and-hashing/easy/majority-element.ts
+++ arrays-and-hashing/easy/majority-element.ts
@@ -3,26 +3,28 @@
 
 function majorityElement(nums: number[]): number {
   const container: { [key: number]: number } = {};
-  let mostCommonNumber: number = 0;
-  let highestCount: number = 0;
+  let majorityElement = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (container[nums[i]] === undefined) {
-      container[nums[i]] = 1;
-    } else {
+    if (container[nums[i]] !== undefined) {
       container[nums[i]] += 1;
+    } else {
+      container[nums[i]] = 1;
     }
   }
 
   for (let key in container) {
-    if (container[key] > highestCount) {
-      mostCommonNumber = Number(key);
-      highestCount = container[key];
-    }
+  if (container[key] > (container[majorityElement] ?? 0)) {
+    majorityElement = Number(key)
   }
-  return mostCommonNumber;
+  }
+
+  return majorityElement
 }
 
-const testNums = [9, 9, 8];
+// Test cases
+const nums11 = [3, 2, 3];
+console.log(majorityElement(nums11)); // Expected: 3
 
-console.log(majorityElement(testNums));
+// const nums2 = [2, 2, 1, 1, 1, 2, 2];
+// console.log(majorityElement(nums2)); // Expected: 2
