42c1809f20f64ce94d5ea2b77fac747baf2c1d4b
Anderson Laventure <Laventurepsu1@gmail.com>
2025-10-09 14:01:06 -0400

    clean up using Map

--- arrays-and-hashing/easy/word-pattern.ts
+++ arrays-and-hashing/easy/word-pattern.ts
@@ -2,38 +2,24 @@
 // https://leetcode.com/problems/word-pattern/
 
 function wordPattern(pattern: string, s: string): boolean {
-  const containerOne: { [key: string]: string | number } = Object.create(null);
-  const containerTwo: { [key: string]: string | number } = Object.create(null);
-  let num: number = 0;
 
-  for (let i = 0; i < pattern.length; i++) {
-    if (containerOne[pattern[i]] === undefined) {
-      containerOne[pattern[i]] = num;
-      containerOne["string"] += "-" + num;
-      num++;
-    } else {
-      const numberNeeded = containerOne[pattern[i]];
-      containerOne["string"] += "-" + numberNeeded;
+    const letterToWord = new Map<string, string>()
+    const wordToLetter= new Map<string, string>()
+
+    const words = s.split(" ")
+
+    if (pattern.length !== words.length) return false
+
+    for (let i = 0; i < words.length; i++) {
+
+        if (wordToLetter.has(words[i]) && pattern[i] !== wordToLetter.get(words[i])) return false
+        if (letterToWord.has(pattern[i]) && words[i] !== letterToWord.get(pattern[i])) return false
+
+        letterToWord.set(pattern[i], words[i])
+        wordToLetter.set(words[i], pattern[i])
     }
-  }
-
-  num = 0;
-
-  const sWord = s.split(" ");
-
-  for (let i = 0; i < sWord.length; i++) {
-    if (containerTwo[sWord[i]] === undefined) {
-      containerTwo[sWord[i]] = num;
-      containerTwo["stringTwo"] += "-" + num;
-      num++;
-    } else {
-      const numberNeeded = containerTwo[sWord[i]];
-      containerTwo["stringTwo"] += "-" + numberNeeded;
-    }
-  }
-
-  return containerOne["string"] === containerTwo["stringTwo"];
-}
+return true
+};
 
 // Test cases
 console.log(wordPattern("abba", "dog cat cat dog")); // Expected: true
