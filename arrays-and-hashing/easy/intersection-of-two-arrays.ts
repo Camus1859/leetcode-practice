9986e5cba1aecb5904a9f5e49fbe1ea84feb6da3
Anderson Laventure <Laventurepsu1@gmail.com>
2025-10-10 15:26:41 -0400

    small clean up

--- arrays-and-hashing/easy/intersection-of-two-arrays.ts
+++ arrays-and-hashing/easy/intersection-of-two-arrays.ts
@@ -2,11 +2,11 @@
 // https://leetcode.com/problems/intersection-of-two-arrays/
 
 const intersections = (nums1: number[], nums2: number[]): number[] => {
-  const container: Map<number, boolean> = new Map();
-  const res: Set<number> = new Set([]);
+  const container: Set<number> = new Set();
+  const res: Set<number> = new Set();
 
   for (const num1 of nums1) {
-    container.set(num1, true);
+    container.add(num1);
   }
 
   for (const num2 of nums2) {
