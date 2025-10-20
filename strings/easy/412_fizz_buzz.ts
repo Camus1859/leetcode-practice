5278abf9ef48135f4da71e848fdf31c648953176
Anderson Laventure <Laventurepsu1@gmail.com>
2025-10-19 21:06:22 -0400

    basic string minipulation

---
+++ strings/easy/412_fizz_buzz.ts
@@ -0,0 +1,21 @@
+// 412. Fizz Buzz
+// https://leetcode.com/problems/fizz-buzz/
+
+function fizzBuzz(n: number): string[] {
+  let res: string[] = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      res.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      res.push("Fizz");
+    } else if (i % 5 === 0) {
+      res.push("Buzz");
+    } else {
+      res.push(i.toString());
+    }
+  }
+  return res;
+}
+
+console.log(fizzBuzz(3)); // Expected: ["1","2","Fizz"]
