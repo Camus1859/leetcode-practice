0c1c69f9ead22c4d09e1091b9f895f83c65589fe
Anderson Laventure <Laventurepsu1@gmail.com>
2025-10-21 16:41:56 -0400

    remove unnecessary map

--- arrays/medium/238_product_of_array_except_self.ts
+++ arrays/medium/238_product_of_array_except_self.ts
@@ -2,28 +2,20 @@
 // https://leetcode.com/problems/product-of-array-except-self/
 
 function productExceptSelf(nums: number[]): number[] {
-  let res: number[] = [];
-  let product: number = 1;
+    let res: number[] = [];
+    let product: number = 1;
 
-  const container: Map<number, number> = new Map();
-
-  for (let i = 0; i < nums.length; i++) {
-    container.set(i, nums[i]);
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    for (const [index, value] of container.entries()) {
-      if (index !== i) {
-        product = product * value;
-      } else {
-        continue;
+    for (let i = 0; i < nums.length; i++) {
+      for (const index in nums) {
+        if (Number(index) !== i) {
+          product = product * nums[Number(index)];
+        }
       }
+      res.push(product);
+      product = 1;
     }
-    res.push(product);
-    product = 1;
-  }
 
-  return res;
+    return res;
 }
 
 console.log(productExceptSelf([1, 2, 3, 4])); // Expected: [24, 12, 8, 6]
