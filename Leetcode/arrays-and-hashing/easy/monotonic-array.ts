77fc1c9e89d784dcd4c24c94f97e1077b3eac316
Anderson Laventure <Laventurepsu1@gmail.com>
2025-10-29 18:30:11 -0400

    tighten up logic

--- Leetcode/arrays-and-hashing/easy/monotonic-array.ts
+++ Leetcode/arrays-and-hashing/easy/monotonic-array.ts
@@ -1,38 +1,27 @@
 // Monotonic Array
 // https://leetcode.com/problems/monotonic-array/
-
 function isMonotonic(nums: number[]): boolean {
+  let isIncreasing = false;
+  let isDecreasing = false;
 
-    let isAlwaysGreater
-    let isAlwaysLesser
-
-    for (let i = 0; i <= nums.length - 2; i++) {
-
-        const currNum = nums[i]
-        const adjacentNum = nums[i + 1]
-
-        if (currNum < adjacentNum) {
-            isAlwaysLesser = true
-        } else if (currNum > adjacentNum) {
-            isAlwaysGreater = true
-        } else {
-            continue
-        }
-
-        if (isAlwaysGreater && currNum < adjacentNum) {
-            return false
-        }
-
-        if (isAlwaysLesser && currNum > adjacentNum) {
-            return false
-        }
-
-
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < nums[i + 1]) {
+      isIncreasing = true;
+      if (isDecreasing) {
+        return false;
+      }
+    } else if (nums[i] > nums[i + 1]) {
+      isDecreasing = true;
+      if (isIncreasing) {
+        return false;
+      }
     }
-    return true
+  }
+
+  return true;
 }
 
 // Test cases
-console.log(isMonotonic([1,2,2,3])); // Expected: true
-console.log(isMonotonic([6,5,4,4])); // Expected: true
-console.log(isMonotonic([1,3,2])); // Expected: false
+console.log(isMonotonic([1, 2, 2, 3])); // Expected: true
+console.log(isMonotonic([6, 5, 4, 4])); // Expected: true
+console.log(isMonotonic([1, 3, 2])); // Expected: false
